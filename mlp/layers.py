Softmax layer implemented but cost incorrect
Softmax layer implemented but there is an issue with the costs.
I think this because I've inherited from Linear which was dealing
with mse only. Cross entropy may be different. Check the cost
grads tomorrow

mlp/layers.py
@@ -110,18 +110,33 @@ class Layer(object):
     
     def bprop(self, h, igrads):
         """
-        Implements a backward propagation through the layer, that is, given
-        h^i denotes the output of the layer and x^i the input, we compute:
-        dh^i/dx^i which by chain rule is dh^i/da^i da^i/dx^i
-        x^i could be either features (x) or the output of the lower layer h^{i-1}
-        :param h: it's an activation produced in forward pass
-        :param igrads, error signal (or gradient) flowing to the layer, note,
-               this in general case does not corresponds to 'deltas' used to update
-               the layer's parameters, to get deltas ones need to multiply it with
-               the dh^i/da^i derivative
+        N.B. Using the notation of res/code_scheme.svg
+        * h^i = f(a^i)            : the output of layer i (f is some function) [1 x K^i vector]
+        * x   = h^{i-1}           : the input to layer i (h^0 = first inputs) [1 x K^{i-1} vector]
+        * a^i = x W^i + b^i       : the activation of the layer [1 x K^i vector]
+        * d^i = g^{i+1} dh^i/da^i : \delta for layer i [1 x K^i vector]
+        * g^i = d^i (W^i).T       : the 'grads' for layer i [1 x K^i vector]
+        
+        Implements a backward propagation from a generic layer i+1 to layer i 
+        i.e. we calculate deltas = d^i and ograds = g^i. The deltas will be
+        used to calculate the update gradient of the Error w.r.t. W^i and b^i:
+            dE/dW^i = h^{i-1} d^i
+            dE/dW^i = h^{i-1} d^i
+        The grads propagate the error back to layer i-1 i.e. the ograds from this
+        call are used as igrads for the next.
+
+        :param h      : h^i - Used to create the function dh^i/da^i
+                        ***even if unused, must be present for consistency amongst layers***
+        :param igrads : g^{i+1} - Used to calculate deltas
+            
         :return: a tuple (deltas, ograds) where:
-               deltas = igrads * dh^i/da^i
-               ograds = deltas \times da^i/dx^i
+            deltas = d^i = igrads  * dh^i/da^i
+                         = g^{i+1} * dh^i/da^i
+            ograds = g^i = dh^i/dx^i
+                         = dh^i/da^i da^i/dx^i
+                         = d^i (W^i).T
+        
+        N.B. da^i/dx^i = d/dx^i (xW^i + b^i) = (W^i).T
         """
         raise NotImplementedError()
 
@@ -176,62 +191,23 @@ class Linear(Layer):
             (self.idim, self.odim))
 
         self.b = numpy.zeros((self.odim,), dtype=numpy.float32)
+    
 
+    def get_name(self):
+        return 'linear'
+    
     def fprop(self, inputs):
-        """
-        Implements a forward propagation through the i-th layer, that is
-        some form of:
-           a^i = xW^i + b^i
-           h^i = f^i(a^i)
-        with f^i, W^i, b^i denoting a non-linearity, weight matrix and
-        biases of this (i-th) layer, respectively and x denoting inputs.
-
-        :param inputs: matrix of features (x) or the output of the previous layer h^{i-1}
-        :return: h^i, matrix of transformed by layer features
-        """
         a = numpy.dot(inputs, self.W) + self.b
         # here f() is an identity function, so just return a linear transformation
         return a
 
     def bprop(self, h, igrads):
-        """
-        Implements a backward propagation through the layer, that is, given
-        h^i denotes the output of the layer and x^i the input, we compute:
-        dh^i/dx^i which by chain rule is dh^i/da^i da^i/dx^i
-        x^i could be either features (x) or the output of the lower layer h^{i-1}
-        :param h: it's an activation produced in forward pass
-        :param igrads, error signal (or gradient) flowing to the layer, note,
-               this in general case does not corresponds to 'deltas' used to update
-               the layer's parameters, to get deltas ones need to multiply it with
-               the dh^i/da^i derivative
-        :return: a tuple (deltas, ograds) where:
-               deltas = igrads * dh^i/da^i
-               ograds = deltas \times da^i/dx^i
-        """
-
         # since df^i/da^i = 1 (f is assumed identity function),
         # deltas are in fact the same as igrads
         ograds = numpy.dot(igrads, self.W.T)
         return igrads, ograds
 
     def bprop_cost(self, h, igrads, cost):
-        """
-        Implements a backward propagation in case the layer directly
-        deals with the optimised cost (i.e. the top layer)
-        By default, method should implement a bprop for default cost, that is
-        the one that is natural to the layer's output, i.e.:
-        here we implement linear -> mse scenario
-        :param h: it's an activation produced in forward pass
-        :param igrads, error signal (or gradient) flowing to the layer, note,
-               this in general case does not corresponds to 'deltas' used to update
-               the layer's parameters, to get deltas ones need to multiply it with
-               the dh^i/da^i derivative
-        :param cost, mlp.costs.Cost instance defining the used cost
-        :return: a tuple (deltas, ograds) where:
-               deltas = igrads * dh^i/da^i
-               ograds = deltas \times da^i/dx^i
-        """
-
         if cost is None or cost.get_name() == 'mse':
             # for linear layer and mean square error cost,
             # cost back-prop is the same as standard back-prop
@@ -241,22 +217,6 @@ class Linear(Layer):
                                       'for the %s cost' % cost.get_name())
 
     def pgrads(self, inputs, deltas):
-        """
-        Return gradients w.r.t parameters
-
-        :param inputs, input to the i-th layer
-        :param deltas, deltas computed in bprop stage up to -ith layer
-        :return list of grads w.r.t parameters dE/dW and dE/db in *exactly*
-                the same order as the params are returned by get_params()
-
-        Note: deltas here contain the whole chain rule leading
-        from the cost up to the the i-th layer, i.e.
-        dE/dy^L dy^L/da^L da^L/dh^{L-1} dh^{L-1}/da^{L-1} ... dh^{i}/da^{i}
-        and here we are just asking about
-          1) da^i/dW^i and 2) da^i/db^i
-        since W and b are only layer's parameters
-        """
-
         grad_W = numpy.dot(inputs.T, deltas)
         grad_b = numpy.sum(deltas, axis=0)
 
@@ -271,11 +231,12 @@ class Linear(Layer):
         self.W = params[0]
         self.b = params[1]
 
-    def get_name(self):
-        return 'linear'
-
         
 class Sigmoid(Linear):
+    
+    def get_name(self):
+        return 'sigmoid'
+    
     ## Showing I could roll my own
     # def sigmoid(self, X):
     #     return 1. / (1 + numpy.exp(-X))
@@ -284,60 +245,31 @@ class Sigmoid(Linear):
         return expit(X)
     
     def fprop(self, inputs):
-        """
-        Implements a forward propagation through the i-th layer, that is
-        some form of:
-           a^i = xW^i + b^i
-           h^i = f^i(a^i)
-        with f^i, W^i, b^i denoting a non-linearity, weight matrix and
-        biases of this (i-th) layer, respectively and x denoting inputs.
-
-        :param inputs: matrix of features (x) or the output of the previous layer h^{i-1}
-        :return: h^i, matrix of transformed by layer features
-        """
         a = super(Sigmoid, self).fprop(inputs)
         return self.sigmoid(a)
     
     def bprop(self, h, igrads):
-        """
-        N.B. Using the notation of res/code_scheme.svg
-        * h^i = f(a^i)            : the output of layer i (f is some function) [1 x K^i vector]
-        * x   = h^{i-1}           : the input to layer i (h^0 = first inputs) [1 x K^{i-1} vector]
-        * a^i = x W^i + b^i       : the activation of the layer [1 x K^i vector]
-        * d^i = g^{i+1} dh^i/da^i : \delta for layer i [1 x K^i vector]
-        * g^i = d^i (W^i).T       : the 'grads' for layer i [1 x K^i vector]
-        
-        Implements a backward propagation from a generic layer i+1 to layer i 
-        i.e. we calculate deltas = d^i and ograds = g^i. The deltas will be
-        used to calculate the update gradient of the Error w.r.t. W^i and b^i:
-            dE/dW^i = h^{i-1} d^i
-            dE/dW^i = h^{i-1} d^i
-        The grads propagate the error back to layer i-1 i.e. the ograds from this
-        call are used as igrads for the next.
-
-        :param h      : h^i - Used to create the function dh^i/da^i
-                        ***even if unused, must be present for consistency amongst layers***
-        :param igrads : g^{i+1} - Used to calculate deltas
-            
-        :return: a tuple (deltas, ograds) where:
-            deltas = d^i = igrads  * dh^i/da^i
-                         = g^{i+1} * dh^i/da^i
-            ograds = g^i = dh^i/dx^i
-                         = dh^i/da^i da^i/dx^i
-                         = d^i (W^i).T
-        
-        N.B. da^i/dx^i = d/dx^i (xW^i + b^i) = (W^i).T
-        """
         # h = Sigmoid(a) = 1. / (1 + numpy.exp(-a))
         # dh/da = numpy.exp(-a) / (1 + numpy.exp(-a))**2
         #       = h(1-h)
         deltas = igrads * h * (1-h)
         ograds = deltas.dot(self.W.T)
-        
         return deltas, ograds
+    
+    def bprop_cost(self, h, igrads, cost):
+        if cost is None or cost.get_name() == 'ce':
+            # for Sigmoid layer and cross entropy cost,
+            # cost back-prop is the same as standard back-prop
+            return self.bprop(h, igrads)
+        else:
+            raise NotImplementedError('Linear.bprop_cost method not implemented '
+                                      'for the %s cost' % cost.get_name())
 
 
 class Softmax(Linear):
+    def get_name(self):
+        return 'softmax'
+    
     def softmax(self, x):
         ## For matrices
         # ex = np.exp(X)
@@ -345,60 +277,31 @@ class Softmax(Linear):
         # assert(tot.shape[0] == ex.shape[0]), \
         #     "Total of exponents should be size N. Sum size %d, N from X is %d" % (tot.shape[0], ex.shape[0])"
         # ex / tot
-        ex = numpy.exp(X)
+        ex = numpy.exp(x)
         tot = numpy.sum(ex)
         return ex / tot
     
     def fprop(self, inputs):
-        """
-        Implements a forward propagation through the i-th layer, that is
-        some form of:
-           a^i = xW^i + b^i
-           h^i = f^i(a^i)
-        with f^i, W^i, b^i denoting a non-linearity, weight matrix and
-        biases of this (i-th) layer, respectively and x denoting inputs.
-
-        :param inputs: matrix of features (x) or the output of the previous layer h^{i-1}
-        :return: h^i, matrix of transformed by layer features
-        """
-        a = super(Sigmoid, self).fprop(inputs)
+        a = super(Softmax, self).fprop(inputs)
         return self.softmax(a)
     
+    # TODO: should probably NotImplementedError bprop and edit bprop_cost
     def bprop(self, h, igrads):
-        """
-        N.B. Using the notation of res/code_scheme.svg
-        * h^i = f(a^i)            : the output of layer i (f is some function) [1 x K^i vector]
-        * x   = h^{i-1}           : the input to layer i (h^0 = first inputs) [1 x K^{i-1} vector]
-        * a^i = x W^i + b^i       : the activation of the layer [1 x K^i vector]
-        * d^i = g^{i+1} dh^i/da^i : \delta for layer i [1 x K^i vector]
-        * g^i = d^i (W^i).T       : the 'grads' for layer i [1 x K^i vector]
-        
-        Implements a backward propagation from a generic layer i+1 to layer i 
-        i.e. we calculate deltas = d^i and ograds = g^i. The deltas will be
-        used to calculate the update gradient of the Error w.r.t. W^i and b^i:
-            dE/dW^i = h^{i-1} d^i
-            dE/dW^i = h^{i-1} d^i
-        The grads propagate the error back to layer i-1 i.e. the ograds from this
-        call are used as igrads for the next.
-
-        :param h      : h^i - Used to create the function dh^i/da^i
-                        ***even if unused, must be present for consistency amongst layers***
-        :param igrads : g^{i+1} - Used to calculate deltas
-            
-        :return: a tuple (deltas, ograds) where:
-            deltas = d^i = igrads  * dh^i/da^i
-                         = g^{i+1} * dh^i/da^i
-            ograds = g^i = dh^i/dx^i
-                         = dh^i/da^i da^i/dx^i
-                         = d^i (W^i).T
-        
-        N.B. da^i/dx^i = d/dx^i (xW^i + b^i) = (W^i).T
-        """
         # h = Softmax(a) = np.exp(a) / np.sum(np.exp(a))
-        # dh_c/da_k = h_c(\dirac_ck - h_k)
+        # dh_c/da_k = h_c(\dirac_ck - h_k)  #NB a matrix
+        # see end of lecture notes mlp05-hid
+        # USE AS TOP LAYER ONLY
+        ograds = numpy.dot(igrads, self.W.T)
+        return igrads, ograds
         
-        deltas = igrads * h * (1-h)
-        ograds = deltas.dot(self.W.T)
-        
+    def bprop_cost(self, h, igrads, cost):
+        if cost is None or cost.get_name() == 'ce':
+            # for softmax layer and cross entropy cost,
+            # cost back-prop is the same as standard back-prop
+            return self.bprop(h, igrads)
+        else:
+            raise NotImplementedError('Linear.bprop_cost method not implemented '
+                                      'for the %s cost' % cost.get_name())
         return deltas, ograds
+
         